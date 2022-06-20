a371447bd8f4df689f7b77a3c34bbc2c6477cddf
Andres Molina <andres.molina@overactive.com>
2022-06-20 12:38:26 -0500

    Test.js: Especificaciones de string

--- test/test.js
+++ test/test.js
@@ -3,3 +3,27 @@ console.log('This is a test file');
 const addResult = 5 + 3;
 
 console.log(`5 + 3 = ${ addResult }`);
+
+// Single line commment
+
+/*
+  Multi line comment
+*/
+
+/**
+ * Multi line comment for documentation
+ * 
+ */
+
+//-----------------------------------------------------------------------------
+//----------------------------------String-------------------------------------
+//-----------------------------------------------------------------------------
+
+// String is more than one character, is an array or a chain of characters
+// String es mas de un caracter, es un arreglo o una cadena de caracteres
+
+let myString = 'This is a chain of characters :D';
+
+console.log(myString);
+console.log('myString lenght: ' + myString.length);
+
