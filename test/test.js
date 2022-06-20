5b3cd333fe1cf1cf9aa735e62d8fce3b3ad94227
Andres Molina <andres.molina@overactive.com>
2022-06-20 12:49:27 -0500

    test.js especificaciones de undefined

--- test/test.js
+++ test/test.js
@@ -21,9 +21,23 @@ console.log(`5 + 3 = ${ addResult }`);
 
 // String is more than one character, is an array or a chain of characters
 // String es mas de un caracter, es un arreglo o una cadena de caracteres
+console.log('----------------------------------String-------------------------------------');
 
 let myString = 'This is a chain of characters :D';
 
 console.log(myString);
 console.log('myString lenght: ' + myString.length);
+
+//-----------------------------------------------------------------------------
+//----------------------------------Undefined----------------------------------
+//----------------------------------------------------------------------------- 
+
+// cuando una variable no se a inicializado, esta variable no se ha definido
+// when a variable has not been initialized, this variable has not been defined 
+
+console.log('----------------------------------Undefined----------------------------------');
+
+let myUndefined;
+console.log(myUndefined);
+
 
